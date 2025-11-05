88d4f088da52526685252e3aa5456acd47bfe2e7
abstraction-builder <167092037+amiran-malania@users.noreply.github.com>
2025-11-05 16:54:46 +0400

    Doced ls.c

--- user/ls.c
+++ user/ls.c
@@ -22,26 +22,31 @@ fmtname(char *path)
   return buf;
 }
 
+// doc : tahes file path as an argument
 void
 ls(char *path)
 {
   char buf[512], *p;
   int fd;
   struct dirent de;
-  struct stat st;
+  struct stat st; // doc : used to extract meta info about a file
 
+  // doc : first, try to open the file
   if((fd = open(path, 0)) < 0){
     fprintf(2, "ls: cannot open %s\n", path);
     return;
   }
 
+  // doc : than, get meta info on that file 
   if(fstat(fd, &st) < 0){
     fprintf(2, "ls: cannot stat %s\n", path);
     close(fd);
     return;
   }
 
+  // doc : figure out what type of file(file or dir) is it and proceed accordingly
   switch(st.type){
+  // doc : pretty straightforward case. if it's a file, just preatty print stats 
   case T_FILE:
     printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
     break;
@@ -51,10 +56,10 @@ ls(char *path)
       printf("ls: path too long\n");
       break;
     }
-    strcpy(buf, path);
-    p = buf+strlen(buf);
-    *p++ = '/';
-    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+    strcpy(buf, path);    // doc : make scope copy of path into buf
+    p = buf+strlen(buf);  // doc : initialize p to point to the END of buf
+    *p++ = '/';           // doc : end buf with '/'
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){ // doc : get meta info on direcotry
       if(de.inum == 0)
         continue;
       memmove(p, de.name, DIRSIZ);
@@ -75,10 +80,12 @@ main(int argc, char *argv[])
 {
   int i;
 
+  // doc : so basically if just 'ls' is run, pass '.' argument to it
   if(argc < 2){
     ls(".");
     exit(0);
   }
+  // doc : if multiple args, run ls for each of them
   for(i=1; i<argc; i++)
     ls(argv[i]);
   exit(0);
